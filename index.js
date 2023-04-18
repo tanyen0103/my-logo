3706b931a6cde62854d54ba82a1a32b2b267acb1
tanyen0103 <tanyen0103@gmail.com>
2023-04-18 22:09:47 +0700

    Firt page my logo

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.getElementById("hi").innerHTML = "The World";
+
+function myFuntion(){
+    document.getElementById("hi").innerHTML = "The World";
+}
+
+function redFuntion(){
+    document.getElementById("changeImg").src = "images/logo-red.png";
+    document.getElementById("title").style = "color: red"
+}
+
+function blueFuntion(){
+    document.getElementById("changeImg").src = "images/logo-blue.png";
+    document.getElementById("title").style = "color: blue"
+}
